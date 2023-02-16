2de1948a8a1172737ed4b04190cd365b2828717d
Jayanth Varma Chennu <jayanth@cintap.com>
2023-02-16 19:59:55 +0530

    test files

--- jenkins/build-insureall.sh
+++ jenkins/build-insureall.sh
@@ -1,17 +1,17 @@
 set +x
 LocalWorkspace=${WORKSPACE}
-WARFilePath="$LocalWorkspace/target/insureall-service.war
+WARFilePath="$LocalWorkspace/target/insureall-service.war"
 echo "WAR File Path is ( Source path from the server) : $WARFilePath"
 
-foldername="Insureall-Service-$(date +"%d-%m-%Y-%H-%M-%S")"
-mkdir -p "$LocalWorkspace/target/insureall-service.war/$foldername"
+foldername="Insureall-$(date +"%d-%m-%Y-%H-%M-%S")"
+mkdir -p "$LocalWorkspace/target/$foldername"
 echo "Folder is created"
 
-echo "Starting - copying file from $WARFilePath to $LocalWorkspace/target/insureall-service.war/$foldername/"
-cp $WARFilePath "$LocalWorkspace/target/insureall-service.war/$foldername/"
-echo "Completed - copying file from $WARFilePath to $LocalWorkspace/target/insureall-service.war/$foldername/"
+echo "Starting - copying file from $WARFilePath to $LocalWorkspace/target/$foldername/"
+cp $WARFilePath "$LocalWorkspace/target/$foldername/"
+echo "Completed - copying file from $WARFilePath to $LocalWorkspace/target/$foldername/"
 
-cd "$LocalWorkspace/target/insureall-service.war/"
+cd "$LocalWorkspace/target/"
 echo "Starting - Upload folder to S3 bucket"
-/usr/local/bin/aws s3 cp $foldername "s3://insureall/Builds/Insureall//$foldername/" --recursive
+/usr/local/bin/aws s3 cp $foldername "s3://insureall/Builds/Insureall/$foldername/" --recursive
 echo "Completed - Upload folder to S3 bucket"
